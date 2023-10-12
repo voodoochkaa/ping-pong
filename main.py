650e98d92441ff9498da0f816f43addc76cf4896
voodoochkaa <kakashka228lollol@gmail.com>
2023-10-12 16:01:03 +0300

    pingpong

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ display.set_icon(image.load('tenis_ball.png'))
 display.set_caption("PING PONG 1VS1")
 window.fill(back)
 
+font.init()
+f1 = font.SysFont(None, 60, bold=True)
+f2 = font.SysFont(None, 50)
+
 class GameSprite(sprite.Sprite):
     # конструктор класу
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -45,17 +49,49 @@ class Player(GameSprite):
 
 racket1 = Player('racket.png', 10, W/3, 50, 150, 5)
 racket2 = Player('racket.png', 700, W/3, 50, 150, 5)
+ball = GameSprite('tenis_ball.png', W/2, H/2, 50, 50, 7)
+
+speed_x = 4
+speed_y = 4
+
+points1 = 0
+points2 = 0
 
 game = True
 finish = False
 while game:
-    time.delay(5)
     window.fill(back)
-    racket1.reset()
-    racket1.update_l()
-    racket2.reset()
-    racket2.update_r()
     for e in event.get():
         if e.type == QUIT:
             game = False
+    points1_txt = f1.render(str(points1), True, (123, 123, 123))
+    points2_txt = f1.render(str(points2), True, (123, 123, 123))
+    window.blit(points1_txt, (50, 20))
+    window.blit(points2_txt, (750, 20))
+    if not finish:
+        time.delay(10)
+        racket1.reset()
+        racket1.update_l()
+        racket2.reset()
+        racket2.update_r()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y < 0 or ball.rect.y > H-50:
+            speed_y *= -1
+        if sprite.collide_rect(ball, racket1) or sprite.collide_rect(ball, racket2):
+            speed_x *= -1
+        
+        if ball.rect.x > W-50:
+            points1 += 1
+            ball.rect.x = W/2
+            ball.rect.y = H/2
+
+        if ball.rect.x < 0:
+            points2 += 1
+            ball.rect.x = W/2
+            ball.rect.y = H/2
+
+        
+
     display.update()
